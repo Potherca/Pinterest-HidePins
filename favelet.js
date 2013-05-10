0441810c83c1bc610a71896cd6c47456c20d7344
Ben Peachey <potherca@gmail.com>
2013-05-10 20:30:31 +0200

    Add rudementary favelet.

---
+++ favelet.js
@@ -0,0 +1,5 @@
+javascript:var%20s=document.createElement('script');
+s.setAttribute('src','http://potherca.github.com/Pinterest-HidePins/PinterestHidePins.js');
+(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(s);
+console.log('Script Tag Added!');void(s);
+
